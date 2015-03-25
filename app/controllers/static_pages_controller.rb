654694c70315c0bd3356cd7ed42e44e3dded172a
GarrusN7 <rsbanjoman@gmail.com>
2015-03-25 17:54:22 -0600

    test integration

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate
+
   def home
     @micropost = current_user.microposts.build if logged_in?
     @feed_items = current_user.feed.paginate(page: params[:page])
@@ -12,4 +14,10 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  private
+
+    def authenticate
+      redirect_to login_path unless current_user
+    end
 end
